2b963fe149535543634e5551c60fdf3367df23f2
Matic Pintarič <matic.pintaric@outlook.com>
2019-06-19 16:31:13 +0200

    Updated setup.

--- setup.py
+++ setup.py
@@ -1,18 +1,12 @@
 from setuptools import setup
 
-with open('README.md') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='evovrp',
     version='1.0',
-    description='Solving vehicle routing problem using evolutionary algorithms.',
-    author='Matic Pintarič',
+    description='Solving MDVRP using evolutionary algorithms.',
+    author='Matic Pintarič, Sašo Karakatič',
     author_email='matic.pintaric@outlook.com',
-    url='https://github.com/karakatic/evovrp',
+    url='https://github.com/mpinta/evovrp',
     license='MIT',
     packages=['evovrp'],
     install_requires=[
